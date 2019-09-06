514efb26f173a5ecc047d2ea00626e66d764fc61
jpearson <jonathan.pearson@arielre.com>
2019-09-06 08:49:21 +0100

    post merge

--- frontend/js/components/button.js
+++ frontend/js/components/button.js
@@ -1,11 +1,18 @@
-function button(label='Submit', id='form', onclick=''){
+function button(label='Submit', id='form', onclick='', icon='plus'){
+    if (typeof icon === 'undefined') { icon = 'plus'; }
+
     var btn = document.createElement("button");
     btn.setAttribute('type', 'button');
     btn.setAttribute('value', label)
     btn.setAttribute('onclick', onclick)
     btn.setAttribute('id', id);
-    btn.setAttribute('class', 'btn btn-primary')
-    btn.innerHTML = label;
+    btn.setAttribute('class', 'btn btn-primary');
     
+    //btn.innerHTML = label;
+    var icn = genIcon(icon);
+    btn.appendChild(icn);
+    
+    btn.insertAdjacentHTML('beforeend', '&nbsp;&nbsp;');
+    btn.insertAdjacentHTML('beforeend', label);
     return(btn)
 }
